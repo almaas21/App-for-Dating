32145a810a2ca968ff5b23b123f1d6fce6738ed0
almaas21 <44881468+almaas21@users.noreply.github.com>
2024-09-27 15:56:14 -0400

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TinderLikeApp from './TinderLikeApp';
+
+function App() {
+  return (
+    <div className="App">
+      <TinderLikeApp />
+    </div>
+  );
+}
+
+export default App;
