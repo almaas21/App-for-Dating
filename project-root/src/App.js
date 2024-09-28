147ecd15ecb2877e97aa7a45b1c80c096239d4bc
almaas21 <44881468+almaas21@users.noreply.github.com>
2024-09-28 11:31:41 -0400

    f

--- project-root/src/App.js
+++ project-root/src/App.js
@@ -4,6 +4,7 @@ import TinderLikeApp from './TinderLikeApp';
 function App() {
   return (
     <div className="App">
+      <h1 className="text-4xl font-bold text-red-500">Debug: React is rendering</h1>
       <TinderLikeApp />
     </div>
   );
