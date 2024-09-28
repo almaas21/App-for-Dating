e0525699c240e563f1bb1f54f0bbc7998e14520d
almaas21 <44881468+almaas21@users.noreply.github.com>
2024-09-28 11:27:19 -0400

    f

--- project-root/src/index.js
+++ project-root/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css'; // Make sure this line is present
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
