69dd077e3d7ad2d1ef27be8eb30f2fce15cb214f
almaas21 <44881468+almaas21@users.noreply.github.com>
2024-09-28 11:36:50 -0400

    Update App.js

--- project-root/src/App.js
+++ project-root/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import TinderLikeApp from './TinderLikeApp';
+      import TinderLikeApp from './TinderLikeApp';
 
-function App() {
-  return (
-    <div className="App">
-      <h1 className="text-4xl font-bold text-red-500">Debug: React is rendering</h1>
-      <TinderLikeApp />
-    </div>
-  );
-}
+      function App() {
+        return (
+          <div className="App">
+            <h1 className="text-4xl font-bold text-red-500">Debug: React is rendering</h1>
+            <div className="text-2xl text-blue-500">
+              TinderLikeApp component below:
+            </div>
+            <TinderLikeApp />
+          </div>
+        );
+      }
 
-export default App;
+      export default App;
